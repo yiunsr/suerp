a34b70ce99a96360e9123230c2589b9655e57a64
yiunsr <yiunsr@hanmail.net>
2023-07-09 16:38:22 +0900

    meta 관련 모델, 스키마 추가

---
+++ app/api_cate_meta/views.py
@@ -0,0 +1,47 @@
+import traceback
+from typing import Any, List
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi import Depends
+from sqlalchemy.orm import Session
+from pydantic import parse_obj_as
+
+from config.db import get_db_session
+from config.http_err import ErrCode
+from config.http_err import ResError
+from config.auth import get_current_active_user
+from app.models.cate_meta import CateMeta
+from app.schemas.cate_meta import CateMetaSchema
+
+from . import api_cate_meta
+
+@api_cate_meta.get("/")
+async def list_obj(db_session: Session = Depends(get_db_session),
+                   _ = Depends(get_current_active_user)) -> Any:
+    db_cate_metas = await CateMeta.listing(db_session)
+    return parse_obj_as(List[CateMetaSchema], db_cate_metas)
+
+@api_cate_meta.get("/{id}")
+async def get_obj(
+        id: int, db_session: Session = Depends(get_db_session),
+        _ = Depends(get_current_active_user)) -> Any:
+    db_obj = await CateMeta.get(db_session, id)
+    if db_obj is None:
+        raise ResError(
+                status_code=404,
+                err_code=ErrCode.NO_ITEM
+            )
+    return parse_obj_as(CateMeta, db_obj)
+
+@api_cate_meta.post("/")
+async def get_obj(
+        db_session: Session = Depends(get_db_session),
+        _ = Depends(get_current_active_user)) -> Any:
+    db_obj = await CateMeta.get(db_session, id)
+    if db_obj is None:
+        raise ResError(
+                status_code=404,
+                err_code=ErrCode.NO_ITEM
+            )
+    return parse_obj_as(CateMeta, db_obj)
+
